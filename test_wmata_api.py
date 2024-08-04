951ed35d50df3d3d7454c4e52080bafc9b604bfc
massoudpourmandi <massoud.pourmandi@gmail.com>
2024-08-04 17:32:24 +0300

    updated

--- test_wmata_api.py
+++ test_wmata_api.py
@@ -7,11 +7,15 @@ class WMATATest(unittest.TestCase):
     def test_http_success(self):
         escalator_response = app.test_client().get('/incidents/escalators').status_code
         # assert that the response code of 'incidents/escalators returns a 200 code
+        self.assertEqual(escalator_response, 200)
 
         elevator_response = app.test_client().get('/incidents/elevators').status_code
         # assert that the response code of 'incidents/elevators returns a 200 code
+        self.assertEqual(elevator_response, 200)
 
-################################################################################
+
+
+    ################################################################################
 
     # ensure all returned incidents have the 4 required fields
     def test_required_fields(self):
@@ -20,7 +24,11 @@ class WMATATest(unittest.TestCase):
         response = app.test_client().get('/incidents/escalators')
         json_response = json.loads(response.data.decode())
 
-        # for each incident in the JSON response assert that each of the required fields
+        for incident in json_response:
+            for field in required_fields:
+                self.assertIn(field, incident)
+
+                # for each incident in the JSON response assert that each of the required fields
         # are present in the response
 
 ################################################################################
@@ -31,8 +39,10 @@ class WMATATest(unittest.TestCase):
         json_response = json.loads(response.data.decode())
 
         # for each incident in the JSON response, assert that the 'UnitType' is "ESCALATOR"
+        for incident in json_response:
+            self.assertEqual(incident["UnitType"], "ESCALATOR")
 
-################################################################################
+        ################################################################################
 
     # ensure all entries returned by the /elevators endpoint have a UnitType of "ELEVATOR"
     def test_elevators(self):
@@ -40,6 +50,8 @@ class WMATATest(unittest.TestCase):
         json_response = json.loads(response.data.decode())
 
         # for each incident in the JSON response, assert that the 'UnitType' is "ELEVATOR"
+        for incident in json_response:
+            self.assertEqual(incident["UnitType"], "ELEVATOR")
 
 ################################################################################
 
--- wmata_api.py
+++ wmata_api.py
@@ -3,7 +3,7 @@ import requests
 from flask import Flask
 
 # API endpoint URL's and access keys
-WMATA_API_KEY = <YOUR_API_KEY_HERE>
+WMATA_API_KEY = "cc07e3358c0540f68e366939d35c331f"
 INCIDENTS_URL = "https://api.wmata.com/Incidents.svc/json/ElevatorIncidents"
 headers = {"api_key": WMATA_API_KEY, 'Accept': '*/*'}
 
@@ -16,9 +16,13 @@ app = Flask(__name__)
 @app.route("/incidents/<unit_type>", methods=["GET"])
 def get_incidents(unit_type):
   # create an empty list called 'incidents'
+  incidents = []
 
   # use 'requests' to do a GET request to the WMATA Incidents API
+  response = requests.get(INCIDENTS_URL, headers=headers)
   # retrieve the JSON from the response
+  data = response.json()
+
 
   # iterate through the JSON response and retrieve all incidents matching 'unit_type'
   # for each incident, create a dictionary containing the 4 fields from the Module 7 API definition
@@ -26,6 +30,21 @@ def get_incidents(unit_type):
   # add each incident dictionary object to the 'incidents' list
 
   # return the list of incident dictionaries using json.dumps()
+
+
+
+  for incident in data['ElevatorIncidents']:
+
+      if incident["UnitType"] == unit_type.upper()[:-1]:
+          incident_data = {
+              "StationCode": incident["StationCode"],
+              "StationName": incident["StationName"],
+              "UnitType": incident["UnitType"],
+              "UnitName": incident["UnitName"],
+          }
+          incidents.append(incident_data)
+
+  return json.dumps(incidents)
 
 if __name__ == '__main__':
     app.run(debug=True)
